bf8c607491720c89add5c4d16d38bee99bda7420
Mathias Erades <mathiaserades@gmail.com>
2025-09-11 01:00:17 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import AddPostes from "../components/AddPoste/AddPoste.vue";
 import PosteDetail from "../components/DetailPoste/DetailPoste.vue";
 
@@ -8,7 +8,7 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes,
 });
 
