c51112bb6a0c34bcf29df321aa1026269cdf4e4e
Rocketphenix <mathiaserades@gmail.com>
2025-09-11 00:38:00 +0200

    Recting

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
-// Remplace "nom-du-repo" par le nom de ton dépôt GitHub
-export default defineConfig({
-	base: "/Site_Candidature/",
-	plugins: [vue()],
-});
+// vue.config.js
+module.exports = {
+	publicPath: "/Site_Candidature/",
+};
