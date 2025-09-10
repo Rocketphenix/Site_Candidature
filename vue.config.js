87330edf81f7a915f9490b30115e5a8624745f6d
Rocketphenix <mathiaserades@gmail.com>
2025-09-11 00:23:54 +0200

    Pushing

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+// Remplace "nom-du-repo" par le nom de ton dépôt GitHub
 export default defineConfig({
 	base: "/Site_Candidature/",
 	plugins: [vue()],
